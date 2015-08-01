028e53299de0786213f4bdda797093d69fe0f664
so-c <so_c321@hotmail.co.jp>
2015-08-01 13:34:44 +0900

    add empty enclosed test.

---
+++ src/test/java/soc/com/github/FooTest.java
@@ -0,0 +1,15 @@
+package soc.com.github;
+
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+@RunWith(Enclosed.class)
+public class FooTest {
+    public static class FooBarTest {
+        
+    }
+    
+    public static class FooBazTest {
+        
+    }
+}
