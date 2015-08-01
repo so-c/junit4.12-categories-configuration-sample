60758c5b75fd23b19118a78e73bd5d06b2c50d8c
so-c <so_c321@hotmail.co.jp>
2015-08-01 13:42:04 +0900

    categorize tests.

--- src/test/java/soc/com/github/FooTest.java
+++ src/test/java/soc/com/github/FooTest.java
@@ -1,11 +1,17 @@
 package soc.com.github;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
+import soc.com.github.categories.Inner;
+import soc.com.github.categories.Outer;
+
 @RunWith(Enclosed.class)
+@Category(Outer.class)
 public class FooTest {
+    @Category(Inner.class)
     public static class FooBarTest {
         @Test
         public void testFooBar() throws Exception {
@@ -18,6 +24,5 @@ public class FooTest {
         public void testFooBaz() throws Exception {
             System.out.println("FooBazTest.");
         }
-
     }
 }
