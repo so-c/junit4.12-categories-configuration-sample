cfe6d5d6a465308f5f1df61313e1be141ff6777f
so-c <so_c321@hotmail.co.jp>
2015-08-01 13:35:37 +0900

    add test methods.

--- src/test/java/soc/com/github/FooTest.java
+++ src/test/java/soc/com/github/FooTest.java
@@ -1,15 +1,23 @@
 package soc.com.github;
 
+import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
 @RunWith(Enclosed.class)
 public class FooTest {
     public static class FooBarTest {
-        
+        @Test
+        public void testFooBar() throws Exception {
+            System.out.println("FooBarTest.");
+        }
     }
-    
+
     public static class FooBazTest {
-        
+        @Test
+        public void testFooBaz() throws Exception {
+            System.out.println("FooBazTest.");
+        }
+
     }
 }
