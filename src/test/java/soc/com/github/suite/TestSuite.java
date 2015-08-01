d74e57ae598addd4065fe00ffe5da272355d57a4
so-c <so_c321@hotmail.co.jp>
2015-08-01 13:50:45 +0900

    add categorized test suite.

---
+++ src/test/java/soc/com/github/suite/TestSuite.java
@@ -0,0 +1,35 @@
+package soc.com.github.suite;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.experimental.categories.Categories.IncludeCategory;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+import soc.com.github.FooTest;
+import soc.com.github.FooTest.FooBarTest;
+import soc.com.github.FooTest.FooBazTest;
+import soc.com.github.categories.Inner;
+import soc.com.github.categories.Outer;
+
+public class TestSuite {
+    @RunWith(Categories.class)
+    @IncludeCategory(Outer.class)
+    @SuiteClasses(FooTest.class)
+    public static class Suite1 {
+        // NoTestsRemainException is thrown.
+    }
+    
+    @RunWith(Categories.class)
+    @IncludeCategory(Outer.class)
+    @SuiteClasses({FooTest.class, FooBarTest.class, FooBazTest.class})
+    public static class Suite2 {
+        // NoTestsRemainException is thrown.
+    }
+    
+    @RunWith(Categories.class)
+    @IncludeCategory(Inner.class)
+    @SuiteClasses({FooBarTest.class, FooBazTest.class})
+    public static class Suite3 {
+        // run testFooBar. not testFooBaz.
+    }
+}
